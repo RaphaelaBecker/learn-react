562a6a097b2a99ad7956ce6fb3b812e8e7de6f45
¨RaBe¨ <raphaela.becker@posteo.de>
2023-06-30 14:08:23 +0200

    feat: shopping cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
 import React, { Component } from "react";
 import Navbar from "./components/navbar";
 import Product from "./components/products";
+import ShoppingCart from "./components/shopping-cart";
 
 class App extends Component {
-  state = {};
+  state = {
+    items: [],
+  };
+
+  addItem = (amount, name, price) => {
+    let currentItems = this.state.items;
+
+    let existingItem = this.state.items.find((item) => item.name === name);
+    if (existingItem) {
+      existingItem.amount++;
+    } else {
+      currentItems.push({
+        amount,
+        name,
+        price,
+      });
+    }
+    this.setState({ items: currentItems });
+  };
+
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <div className="fruit-container">
-          <Product
-            title="Orangen"
-            img="/assets/oranges.jpg"
-            description="Unsere Orangen wurden frisch aus Übersee eingeflogen und werden reif und saftig geliefert"
-          />
-          <Product
-            title="Äpfel"
-            img="/assets/apple.jpg"
-            description="Die schönsten Äpfel aus Deutschland"
-          />
-          <Product
-            title="Birnen"
-            img="/assets/pears.jpg"
-            description="Birnen direkt vom Baum ihn ihren Mund"
-          />
-          <Product
-            title="Himbeere"
-            img="/assets/raspberries.jpg"
-            description="Süße kleine Verführungen, die am besten in wunderbaren Momenten genosst werden"
-          />
+        <div className="main-container">
+          <div className="fruit-container">
+            <Product
+              onAdd={() => this.addItem(1, "Orange", 2.99)}
+              title="Orangen"
+              img="/assets/oranges.jpg"
+              description="Unsere Orangen wurden frisch aus Übersee eingeflogen und werden reif und saftig geliefert"
+            />
+            <Product
+              onAdd={() => this.addItem(1, "Äpfel", 4.99)}
+              title="Äpfel"
+              img="/assets/apple.jpg"
+              description="Die schönsten Äpfel aus Deutschland"
+            />
+            <Product
+              onAdd={() => this.addItem(1, "Birnen", 4.99)}
+              title="Birnen"
+              img="/assets/pears.jpg"
+              description="Birnen direkt vom Baum ihn ihren Mund"
+            />
+            <Product
+              onAdd={() => this.addItem(1, "Himbeeren", 8.99)}
+              title="Himbeeren"
+              img="/assets/raspberries.jpg"
+              description="Süße kleine Verführungen, die am besten in wunderbaren Momenten genosst werden"
+            />
+          </div>
+          <ShoppingCart items={this.state.items} />
         </div>
       </React.Fragment>
     );
